4911ab1a3a933268d7b22613e06c9bd4110e9f33
rutuja70 <rutujahegade7@gmail.com>
2025-08-12 13:32:54 +0530

    hi

--- src/test/java/demo.java
+++ src/test/java/demo.java
@@ -3,7 +3,8 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("hello world");
+		System.out.print("hello world");
+		System.out.print("hello 1world");
 	}
 
 }
